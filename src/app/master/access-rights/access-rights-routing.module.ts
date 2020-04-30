c830a61bcb22907203675839ed2075c512ce7372
kisalay-kumar <kisuvisu95@gmail.com>
2020-04-30 02:45:44 -0700

    Space

--- src/app/master/access-rights/access-rights-routing.module.ts
+++ src/app/master/access-rights/access-rights-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AccessRightsComponent } from './access-rights.component';
 const routes: Routes = [{ path: '', component: AccessRightsComponent }];
-
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
